16dff4baf9d8757c02a70d41bec2cda1bc224670
Staci Genesi <sgenesi@hotmail.com>
2021-01-16 14:02:52 -0700

    finished site

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,10 @@
 // create the about section
 const generateAbout = aboutText => {
-    if (!aboutText) {
-        return '';
-    }
+  if (!aboutText) {
+    return '';
+  }
 
-    return `
+  return `
       <section class="my-3" id="about">
         <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
         <p>${aboutText}</p>
@@ -14,14 +14,14 @@ const generateAbout = aboutText => {
 
 // create the projects section
 const generateProjects = projectsArr => {
-    return `
+  return `
       <section class="my-3" id="portfolio">
         <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
         <div class="flex-row justify-space-between">
         ${projectsArr
-            .filter(({ feature }) => feature)
-            .map(({ name, description, languages, link }) => {
-                return `
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return `
             <div class="col-12 mb-2 bg-dark text-light p-3">
               <h3 class="portfolio-item-title text-light">${name}</h3>
               <h5 class="portfolio-languages">
@@ -32,13 +32,13 @@ const generateProjects = projectsArr => {
               <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
           `;
-            })
-            .join('')}
+      })
+      .join('')}
   
         ${projectsArr
-            .filter(({ feature }) => !feature)
-            .map(({ name, description, languages, link }) => {
-                return `
+      .filter(({ feature }) => !feature)
+      .map(({ name, description, languages, link }) => {
+        return `
             <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
               <h3 class="portfolio-item-title text-light">${name}</h3>
               <h5 class="portfolio-languages">
@@ -49,8 +49,8 @@ const generateProjects = projectsArr => {
               <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
           `;
-            })
-            .join('')}
+      })
+      .join('')}
         </div>
       </section>
     `;
@@ -58,10 +58,10 @@ const generateProjects = projectsArr => {
 
 // export function to generate entire page
 module.exports = templateData => {
-    // destructure page data by section
-    const { projects, about, ...header } = templateData;
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
 
-    return `
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -80,7 +80,7 @@ module.exports = templateData => {
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
           <nav class="flex-row">
             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github
-        }">GitHub</a>
+    }">GitHub</a>
           </nav>
         </div>
       </header>
